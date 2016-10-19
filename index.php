271eae562dee0e6e4674e9fa3ccd35b5abb17e03
himanshu6785 <prabhjotelance@gmail.com>
2016-10-19 16:59:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,47 +25,53 @@
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Container Div Starts -->
 <div class="container">
-<form name="routeForm" id="routeForm">
-	<div>
-		<span>Departure City</span>    
-		<select name="departure_city" id="departure_city">
-			<option value="">--Departure City--</option>
-			<option value="London">London</option>
-			<option value="Amsterdam">Amsterdam</option>
-			<option value="Warsaw">Warsaw</option>
-			<option value="Stockholm">Stockholm</option>
-		</select>
-	</div>
-    
-	<p>&nbsp;</p>
+	<!-- Form Starts -->
+	<form name="routeForm" id="routeForm">
+		<div>
+			<span>Departure City</span>    
+			<select name="departure_city" id="departure_city">
+				<option value="">--Departure City--</option>
+				<option value="London">London</option>
+				<option value="Amsterdam">Amsterdam</option>
+				<option value="Warsaw">Warsaw</option>
+				<option value="Stockholm">Stockholm</option>
+			</select>
+		</div>
 
-	<div>
-		<span>Arrival City</span>
-		<select name="arrival_city" id="arrival_city">
-			<option value="">--Arrival City--</option>
-			<option value="London">London</option>
-			<option value="Amsterdam">Amsterdam</option>
-			<option value="Warsaw">Warsaw</option>
-			<option value="Stockholm">Stockholm</option>
-		</select>
-	</div>
+		<p>&nbsp;</p>
 
-	<p>&nbsp;</p>
+		<div>
+			<span>Arrival City</span>
+			<select name="arrival_city" id="arrival_city">
+				<option value="">--Arrival City--</option>
+				<option value="London">London</option>
+				<option value="Amsterdam">Amsterdam</option>
+				<option value="Warsaw">Warsaw</option>
+				<option value="Stockholm">Stockholm</option>
+			</select>
+		</div>
+
+		<p>&nbsp;</p>
+
+	    <input type="radio" name="sort_route" value="Cheapest" onclick="getBestRoutes();" checked> Cheapest
+	    <input type="radio" name="sort_route" value="Fastest" onclick="getBestRoutes();"> Fastest
+
+		<p>&nbsp;</p>
+		<div>
+			<input type="button" value="Search" id="search" onclick="getBestRoutes();">
+			<input type="button" value="Reset" id="reset" onclick="clearForm();">
+		</div>
+
+	</form>
+	<!-- Container Div Ends -->
 	
-    <input type="radio" name="sort_route" value="Cheapest" onclick="getBestRoutes();" checked> Cheapest
-    <input type="radio" name="sort_route" value="Fastest" onclick="getBestRoutes();"> Fastest
-    
-	<p>&nbsp;</p>
-	<div>
-		<input type="button" value="Search" id="search" onclick="getBestRoutes();">
-		<input type="button" value="Reset" id="reset" onclick="clearForm();">
-	</div>
+	<!-- Response Div -->
+	<div id="result_div"></div>
 	
-</form>
-
-<div id="result_div"></div>
 </div>
+<!-- Container Div Ends -->
 
 <!------------- Script Begins ------------------>
 <script type="text/javascript">
